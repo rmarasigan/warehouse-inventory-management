36784ee6c155c10691a269c32b29cb2ed28802a6
rmarasigan <russianhielle@gmail.com>
2024-07-31 13:59:59 +0800

    add: api request validator

---
+++ api/schema/validator/user.go
@@ -0,0 +1,57 @@
+package validator
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/rmarasigan/warehouse-inventory-management/internal/utils/log"
+	"github.com/xeipuuv/gojsonschema"
+)
+
+// The base path for the schema validator specification JSON files.
+const basePath = "/api/schema/validator/spec/"
+
+// isValid validates the input JSON against the specified JSON schema.
+//
+// Parameters:
+//   - input:  A byte slice containing the JSON data to be validated.
+//   - source: The path to the JSON schema file.
+func isValid(input []byte, source string) (bool, []string) {
+	var (
+		errors   []string
+		pwd, _   = os.Getwd()
+		request  = gojsonschema.NewBytesLoader(input)
+		document = gojsonschema.NewReferenceLoader("file://" + pwd + source)
+	)
+
+	schema, err := gojsonschema.NewSchema(document)
+	if err != nil {
+		log.Error(err.Error(), slog.String("request", string(input)), slog.String("source", source))
+		return false, nil
+	}
+
+	result, err := schema.Validate(request)
+	if err != nil {
+		log.Error(err.Error(), slog.String("request", string(input)), slog.String("source", source))
+		return false, nil
+	}
+
+	if result == nil || result.Valid() {
+		return true, nil
+	}
+
+	for _, err := range result.Errors() {
+		errors = append(errors, err.Description())
+	}
+
+	return false, errors
+}
+
+// ValidateUser validates the input JSON against the users schema.
+//
+// Returns:
+//   - bool:			'true' if the input is valid, 'false' otherwise.
+//   - []string:  A list of error message if the validation fails.
+func ValidateUser(input []byte) (bool, []string) {
+	return isValid(input, basePath+"users.json")
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,11 @@ go 1.21
 require (
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/jmoiron/sqlx v1.4.0
+	github.com/xeipuuv/gojsonschema v1.2.0
 )
 
-require filippo.io/edwards25519 v1.1.0 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+)
